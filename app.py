8b6d811799d9fbd9733b6dc4f3713503cca03005
Tahsin Ahmed <tahsin4466@g.ucla.edu>
2024-02-08 16:28:18 -0800

    Templates folder and main.html setup for basic landing page.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return flask.render_template('main.html')
 
 
 if __name__ == '__main__':
