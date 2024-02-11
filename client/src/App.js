ffb38358ce1ecc0f6b47c9e898ad7813807317c3
Tahsin Ahmed <tahsin4466@g.ucla.edu>
2024-02-10 17:11:18 -0800

    Addition of MUI component and test button

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bruin Bites front-end in progress!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="contained" color="primary">
+          Hi!
+        </Button>
       </header>
     </div>
   );
