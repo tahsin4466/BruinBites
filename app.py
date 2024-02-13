f0d6e97b918030bc462b73639eb5109dc997402f
Tahsin Ahmed <tahsin4466@g.ucla.edu>
2024-02-13 01:07:00 -0800

    Transition of JS components to TypeScript.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import flask
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
+from flask import CORS
 
 app = Flask(__name__, static_folder='client/build', static_url_path='')
+CORS(app)
 
 
 @app.route('/')
