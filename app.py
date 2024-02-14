08e60ee01805a47bf3f55097366229c853b991ae
Tahsin Ahmed <tahsin4466@g.ucla.edu>
2024-02-14 11:37:09 -0800

    Addition of Header component, organized project structure

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import flask
 from flask import Flask, send_from_directory, jsonify
-from flask import CORS
+from flask_cors import CORS
 
 app = Flask(__name__, static_folder='client/build', static_url_path='')
 CORS(app)
