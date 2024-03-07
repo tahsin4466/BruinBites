2c5227e9c73d60e568ee183db8af8fc3781953ac
PrincDani <j.dragoon.c@gmail.com>
2024-03-06 23:32:24 -0800

    added "user profile" to the user profile page

--- client/src/pages/User.tsx
+++ client/src/pages/User.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import HeaderMenu from '../components/HeaderMenu';
+import { Box, Typography } from '@mui/material';
 
 function User() {
-  return <HeaderMenu></HeaderMenu>
+  return (
+    <>
+    <div style={{ height: '100vh', overflow: 'auto' }}>
+      <Box style={{ position: 'fixed', width: '100%', zIndex: 1000 }}>
+        <HeaderMenu />
+      </Box>
+      <Box p={2} mt={4}> {/* Add mt={12} to create space below the fixed header */}
+        <Typography variant="h4" align="center" mt={4}>
+          User Profile
+        </Typography>
+        {/* Add user profile content here */}
+      </Box>
+    </div>
+    </>
+  );
 }
 
 export default User;
