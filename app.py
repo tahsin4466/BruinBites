1ca472f54f39f4c169b933c2a82667599c2a9958
Tahsin Ahmed <tahsin4466@g.ucla.edu>
2024-03-10 15:44:45 -0700

    Indentation error fix

--- app.py
+++ app.py
@@ -310,24 +310,25 @@ def login():
 
 @app.route('/api/signup', methods=['POST'])
 def signup():
-  firstName = data.get('firstName')
-  lastName = data.get('lastName')
-  email = data.get('email')
-  password = data.get('password')
-  ID = randint(10000000, 99999999)
-  dateToday = date.today()
-  #Regex pattern for valid email
-  if re.match(r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email) is None:
+    data = request.get_json()
+    firstName = data.get('firstName')
+    lastName = data.get('lastName')
+    email = data.get('email')
+    password = data.get('password')
+    ID = randint(10000000, 99999999)
+    dateToday = date.today()
+    #Regex pattern for valid email
+    if re.match(r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email) is None:
       message = jsonify({'message': 'Invalid email', 'status': 'failure'}), 400
-  #Regex patterns for valid First and Last names
-  elif re.match(r'^[A-Z][a-z]+(?: [A-Z][a-z]+)*$|^([A-Z][a-z]+(?:[-\'][A-Z][a-z]+)*)+$', firstName) is None:
+    #Regex patterns for valid First and Last names
+    elif re.match(r'^[A-Z][a-z]+(?: [A-Z][a-z]+)*$|^([A-Z][a-z]+(?:[-\'][A-Z][a-z]+)*)+$', firstName) is None:
       message = jsonify({'message': 'First name must be properly formatted (capital, no numbers etc.)', 'status': 'failure'}), 400
-  elif re.match(r'^[A-Z][a-z]+(?: [A-Z][a-z]+)*$|^([A-Z][a-z]+(?:[-\'][A-Z][a-z]+)*)+$', lastName) is None:
+    elif re.match(r'^[A-Z][a-z]+(?: [A-Z][a-z]+)*$|^([A-Z][a-z]+(?:[-\'][A-Z][a-z]+)*)+$', lastName) is None:
       message = jsonify({'message': 'Last name must be properly formatted (capital, no numbers etc.)', 'status': 'failure'}), 400
-  #Regex pattern for strong password
-  elif re.match(r'^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$', password) is None:
+    #Regex pattern for strong password
+    elif re.match(r'^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$', password) is None:
       message = jsonify({'message': 'Password must be at least 8 characters long, with a capital and special character', 'status': 'failure'}), 400
-  else:
+    else:
       db_connection = dbConnect()
       try:
           with db_connection.cursor() as cursor:
@@ -337,25 +338,13 @@ def signup():
               if len(row) > 0:
                   message = jsonify({'message': 'Email exists', 'status': 'failure'}), 400
               else:
-                  sql = "INSERT INTO BB_User (User_ID, First_Name, Last_Name, User_PFP, Email, Date_Joined, Password) VALUES (%s, %s, %s, 'https://example.com/user7.jpg', %s, %s, %s)"
-                  cursor.execute(sql, (ID, firstName, lastName, email, dateToday, password,))
-                  db_connection.commit()
-                  message = jsonify({'message': 'Sign up successful', 'status': 'success'}), 201
-        finally:
+                sql = "INSERT INTO BB_User (User_ID, First_Name, Last_Name, User_PFP, Email, Date_Joined, Password) VALUES (%s, %s, %s, 'https://example.com/user7.jpg', %s, %s, %s)"
+                cursor.execute(sql, (ID, firstName, lastName, email, dateToday, password,))
+                db_connection.commit()
+                message = jsonify({'message': 'Sign up successful', 'status': 'success'}), 201
+      finally:
             db_connection.close()
     return message
-
-@app.route('/')
-def homePage():
-    return send_from_directory(app.static_folder, 'index.html')
-
-@app.route('/restaurant')
-def restaurantPage():
-    return send_from_directory(app.static_folder, 'index.html')
-
-@app.route('/login')
-def loginPage():
-    return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/api/checkReviewStatus', methods=['GET'])
 def checkReviewStatus():
